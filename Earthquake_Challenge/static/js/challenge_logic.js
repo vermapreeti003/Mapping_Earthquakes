083f1576a7fd39d058efcd3a4ec1a9dc9d9eb1d1
vermapreeti003 <vermapreeti003@gmail.com>
2022-11-28 09:38:37 -0500

    Adding all the layers and creating map

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -14,7 +14,12 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 	maxZoom: 18,
 	accessToken: API_KEY
 });
-
+// We create the third tile layer that will be the background of our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
 	center: [40.7, -94.5],
@@ -25,7 +30,8 @@ let map = L.map('mapid', {
 // Create a base layer that holds all three maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite": satelliteStreets
+  "Satellite": satelliteStreets,
+  "Dark": dark
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
@@ -37,7 +43,8 @@ let majorEQ = new L.LayerGroup();
 let overlays = {
   "Earthquakes": allEarthquakes,
   "Tectonic Plates": tectonicPlates,
-  "Major Earthquakes": majorEQ
+  "Major Earthquakes": majorEQ,
+  "Dark": dark
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -97,7 +104,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     	pointToLayer: function(feature, latlng) {
       		console.log(data);
       		return L.circleMarker(latlng);
-        },
+      },
       // We set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
      // We create a popup for each circleMarker to display the magnitude and location of the earthquake
@@ -109,7 +116,60 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
   // Then we add the earthquake layer to our map.
   allEarthquakes.addTo(map);
-
+// 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
+// 4. Use the same style as the earthquake data.  
+function styleInfo(feature){
+    return {
+      opacity: 1,
+      fillOpacity:1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight:0.5
+    };
+}
+  
+// 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+function getColor(magnitude){
+  if (magnitude >= 6){
+    return "#ea2c2c";
+  }
+  if (magnitude >= 5){
+    return "#ea822c";
+  }
+  if (magnitude < 5){
+    return "#ee9c00";
+  }
+}
+  
+  // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+function  getRadius(magnitude){
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+  
+  // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
+  // sets the style of the circle, and displays the magnitude and location of the earthquake
+  //  after the marker has been created and styled.
+  L.geoJson(data, {
+    pointToLayer: function(feature,latlng){
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature,layer){
+    layer.bindPopup("Magnitude: "+ feature.properties.mag + "<br>Location: "+feature.properties.place);  
+    }
+      
+  }).addTo(majorEQ);
+  // 8. Add the major earthquakes layer to the map.
+  majorEQ.addTo(map);
+  // 9. Close the braces and parentheses for the major earthquake data.
+  });
   // Here we create a legend control object.
 let legend = L.control({
   position: "bottomright"
